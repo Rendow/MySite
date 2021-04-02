55117e861e105a3f2b5cf7655fd09013abc73b0f
Rendow <rendow33@gmail.com>
2021-04-02 22:44:00 +0300

    commit

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import s from './Skills.module.css';
 import Container from '../common/styles/Container.module.css';
+import Skill from "./skill/Skill";
 
 function Skills() {
+    let contClass = Container.container + ' ' + s.skillsContainer
     return (
-        <div className={s.mainBlock}>
+        <div className={s.skillsBlock}>
+            <div className={contClass}>
+                <h2 className={s.title}> Skills</h2>
+                <div className={s.skills}>
 
+                    <Skill title={'js'} description={'  Lorem ipsum dolor sit aur'}/>
+                    <Skill title={'ts'} description={'  beatae consectetur deleniti,  molestiae porro quibusdam quod rem tempora temporibus velit voluptates, voluptatum.\n'}/>
+                    <Skill title={'react'} description={'met, consectetur adipisicing elit. Aperiam aspernat'}/>
+                    <Skill title={'css'} description={'eius expedita explicabo fugiat in iste magnam'}/>
+                </div>
 
+            </div>
         </div>
     );
 }
---
+++ src/skills/skill/Skill.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from './Skill.module.css';
+
+
+type SkillPropsType = {
+    title:string
+    description:string
+}
+
+function Skill(props: SkillPropsType) {
+    return (
+        <div className={s.skill}>
+
+            <div className={s.icon}> </div>
+            <div>
+                <h3> {props.title}</h3>
+            </div>
+            <div className={s.text}>{props.description} </div>
+        </div>
+    );
+}
+
+export default Skill;
